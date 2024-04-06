33c326940b287c17102087178f094e1ea7322eb8
oscar-cortez-home <oscarkortez@gmail.com>
2024-04-06 10:51:28 -0400

    installed tailwind with preprocessor website steps

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { FunctionComponent, ReactNode } from 'react'
 export const App: FunctionComponent = (): ReactNode => {
   return (
     <>
-      <h1>Hi</h1>
+      <h1 className="text-3xl font-bold underline text-red-200">
+        Hi, how are you
+      </h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 
+import './main.css'
+
 const root = document.getElementById('root')!
 
 createRoot(root).render(
