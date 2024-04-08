7e6d3728163fd4e941068ab56a2a39cc75d508eb
oscar-cortez-home <oscarkortez@gmail.com>
2024-04-08 17:18:41 -0400

    fixed input Text component

--- src/components/Form/InputText/InputText.tsx
+++ src/components/Form/InputText/InputText.tsx
@@ -6,19 +6,17 @@ type InputTextProps = {
   icon?: IconType
   register: ReturnType<typeof useForm>['register']
   type: 'text' | 'email'
-  // value?: string
   id?: string
   placeholder: string
   disabled?: boolean
-  // onChange: (event: ChangeEvent<HTMLInputElement>) => void
+  name: string
 }
 
 export const InputText: FunctionComponent<InputTextProps> = ({
   icon: Icon,
   register,
-  type,
-  id,
   disabled,
+  name,
   ...props
 }): ReactNode => {
   return (
@@ -29,9 +27,8 @@ export const InputText: FunctionComponent<InputTextProps> = ({
         )}
         <input
           className="w-full border border-gray-300 outline-none py-2 pl-7 pr-2 rounded-lg transition-all duration-300 hover:border-sky-500  focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(186,230,253,1)]"
-          id={id || type}
           disabled={disabled}
-          {...register(id || type)}
+          {...register(name)}
           {...props}
         />
       </div>
